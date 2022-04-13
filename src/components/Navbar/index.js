100b7468dc51edb0902d269883bd03aab8483cd3
javascript4breakfast <49762980+javascript4breakfast@users.noreply.github.com>
2022-04-13 15:25:55 -0700

    ui controls update

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,14 +6,20 @@ import withAuthorization from '../../session/withAuthorization';
 import { withFirebase } from '../../firebase';
 import { compose } from 'recompose';
 import withRouter from '../../session/withRouter';
+import useUIControls from '../../hooks/useUIControls';
+import { AppContext } from '../../context';
+import { GiSunrise, GiSunset, GiResize } from 'react-icons/gi';
 
 const NavBarWrapper = styled.nav`
   box-shadow: 0px 1px 8px 1px rgba(0, 0, 0, 0.2);
   display: flex;
   flex-direction: column;
 `;
-
+const ThemeButtonContainer = styled.div`
+`;
 const NavMenuWrapper = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 const NavMenu = ({ isLoggedIn }) => {
@@ -23,10 +29,16 @@ const NavMenu = ({ isLoggedIn }) => {
     </NavMenuWrapper>
   )
 }
+const Flexer = styled.div`
+  display: flex;
+`;
 
 const NavControls = styled.div`
   display: flex;
+  margin: 0 16px;
   justify-content: space-between;
+  align-items: center;
+
   ._btn {
     display: flex;
     align-items: center;
@@ -41,7 +53,6 @@ const NavControls = styled.div`
     grid-columns-gap: 8px;
     grid-row-gap: 8px;
     align-content: center;
-
   }
 `
 const AuthedNavWrapper = styled.div`
@@ -80,26 +91,48 @@ const NonAuthedNav = ({ navigate }) => {
   const goToSignUpPage = () => navigate('/sign-up');
 
   return (
-    <div className='_btn'>
-      <ActionButton isQuiet onPress={goToSignUpPage}>
-          Sign-up
-      </ActionButton>
-    </div>
+    <Flexer>
+      <div className='_btn'>
+        <ActionButton isQuiet onPress={goToSignUpPage}>
+            Sign-up
+        </ActionButton>
+      </div>
+      <div className='_btn'>
+        <ActionButton isQuiet onPress={goToSignInPage}>
+            Sign-in
+        </ActionButton>
+      </div>
+    </Flexer>
   )
 }
 
 const NavBarBase = ({ firebase, navigate }) => {
+  const [state] = React.useContext(AppContext);
   const [open, setOpen] = React.useState(false);
+  const {toggleTheme, reSize} = useUIControls();
   const currentUser = firebase.auth?.currentUser;
   const isLoggedIn = !!currentUser;
+  const { theme } = state;
   return (
     <>
       <NavBarWrapper>
         <NavControls>
-          <div>
-            <HamburgerButton open={open} setOpen={setOpen} />
-          </div>
-          {currentUser ? <AuthedNav navigate={navigate} firebase={firebase} currentUser={currentUser} /> : <NonAuthedNav navigate={navigate} />}
+          <NavMenuWrapper>
+            <div>
+              <HamburgerButton open={open} setOpen={setOpen} />
+            </div>
+            <div>
+              {currentUser ? <AuthedNav navigate={navigate} firebase={firebase} currentUser={currentUser} /> : <NonAuthedNav navigate={navigate} />}
+            </div>
+          </NavMenuWrapper>
+          <ThemeButtonContainer>
+            <ActionButton onPress={reSize} isQuiet>
+              <GiResize />
+            </ActionButton>
+            <ActionButton onPress={toggleTheme} isQuiet>
+              {theme === 'light' ? <GiSunset /> : <GiSunrise />}
+            </ActionButton>
+          </ThemeButtonContainer>
         </NavControls>
         {open && (
           <NavMenu isLoggedIn={isLoggedIn} />
