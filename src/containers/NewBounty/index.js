f7e0a63f66df934de4159bdbd8f8b637fd7c26e0
javascript4breakfast <49762980+javascript4breakfast@users.noreply.github.com>
2022-04-20 19:23:41 -0700

    merge w/eric changes

--- src/containers/NewBounty/index.js
+++ src/containers/NewBounty/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import {Button, ComboBox, Item, ProgressCircle, TextArea, TextField} from '@adobe/react-spectrum'
+import {
+  Button,
+  ComboBox,
+  Item,
+  ProgressCircle,
+  Picker,
+  TextArea,
+  TextField
+} from '@adobe/react-spectrum'
+import {FcFullTrash} from 'react-icons/fc';
 import axios from 'axios';
 import styled from 'styled-components';
 import withAuthorization from '../../session/withAuthorization';
 import { withFirebase } from '../../firebase';
+import { AppContext } from '../../context';
+
 import { compose } from 'recompose';
 import withRouter from '../../session/withRouter';
 
@@ -14,19 +25,51 @@ const BountyFormWrapper = styled.div`
   margin: 16px;
 `;
 
+const TopicsWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  div {
+    padding: 8px 8px 8px 0;
+  }
+`;
+
 const initialState = {
   categories: null,
   loading: true,
   error: null,
   selected: null,
   valid: false,
-}
+};
 
+const sanitizeByKey = (arr, key) => [
+  ...new Map(arr.map((item) =>  [item[key], item])).values(),
+];
 
-function NewBountyBase() {
+function NewBountyBase({ firebase, navigate }) {
   const [state, setState] = React.useState({ ...initialState });
-  const [categoryId, setCategoryId] = React.useState();
-  const [subCategoryId, setSubCategoryId] = React.useState();
+  const [_state] = React.useContext(AppContext);
+  const [categoryId, setCategoryId] = React.useState(null);
+  const [subCategoryId, setSubCategoryId] = React.useState(null);
+  const [topics, setTopics] = React.useState([]);
+  const [selectedTopics, setSelectedTopics] = React.useState([]);
+  const [description, setDescription] = React.useState('');
+  const [requirements, setRequirements] = React.useState('');
+  const [authUserId, setAuthUserId] = React.useState(null);
+  React.useEffect(() => {
+    if (_state.authUser && _state.authUser.uid) {
+      setAuthUserId(_state.authUser.uid);
+    }
+  }, [_state.authUser]);
+  console.log('authUserId =>', authUserId);
+  React.useEffect(() => {
+    if (subCategoryId && state.selected.subCategories) {
+      const [results] = state.selected.subCategories.filter(x => x.subCategoryId === subCategoryId);
+      setTopics(results.topics);
+    }
+    if (selectedTopics && selectedTopics.length) {
+      setSelectedTopics([]);
+    }
+  }, [subCategoryId]);
 
   React.useEffect(() => {
     const categoriesUrl = `http://localhost:4000/categories`;
@@ -42,8 +85,6 @@ function NewBountyBase() {
               categories,
               loading
             }))
-
-            console.log('categories =>', categories);
           }
         })
         .catch((error) => {
@@ -58,17 +99,71 @@ function NewBountyBase() {
 
   React.useEffect(() => {
     let isSubsribed = true;
+    if (selectedTopics && selectedTopics.length) setSelectedTopics([]);
+    if (topics?.length) setTopics([]);
 
     if (state.categories && isSubsribed) {
       const [selected] = state.categories.filter((x) => x.categoryId === categoryId);
       setState((prevState) => ({
         ...prevState,
         selected,
-      }))
+      }));
     }
 
     return () => isSubsribed = false;
   }, [categoryId]);
+
+  const handleTopicChange = topicId => {
+    const filtered = topics.filter(x => x.topicId === topicId);
+    const merged = [
+      ...selectedTopics,
+      ...filtered
+    ];
+    const cleanTopics = sanitizeByKey(merged, 'topicId');
+    setSelectedTopics(cleanTopics);
+  }
+
+  const handleDeleteTopic = topicId => {
+    const filtered = selectedTopics.filter((x) => x.topicId !== topicId);
+    setSelectedTopics(filtered);
+  }
+
+  const handleDescChange = (value) => {
+    setDescription(value);
+  }
+
+
+  const handleReqChange = (value) => {
+    setRequirements(value);
+  }
+
+  const handleSubmitNewBounty = () => {
+    const toppers = selectedTopics.map(x => x.topicId);
+
+    //http://localhost:4000/users/${authUserId}
+    axios.get(`http://localhost:4000/users/${authUserId}`)
+      .then((response) => {
+        if (response.data && response.data.value) {
+          const { data: { value } } = response;
+          const [result] = value;
+          const blobject = {
+            categoryId: [categoryId],
+            subCategoryId: [subCategoryId],
+            description,
+            requirements: [requirements],
+            topicIds: [...new Set(toppers)],
+            accountId: result.accountid,
+          }
+
+          console.log('use this for next api call, blobject =>', blobject);
+        }
+      })
+      .catch((error) => {
+        console.log('there was error:', error);
+      })
+    console.log('blobject =>', blobject);
+
+  }
 
   if (state.loading) {
     return (
@@ -77,7 +172,6 @@ function NewBountyBase() {
       </div>
     );
   }
-  console.log('selected =>', state.selected);
   return (
     <BountyFormWrapper>
         <div>
@@ -99,11 +193,33 @@ function NewBountyBase() {
             {(item) => <Item key={item.subCategoryId}>{item.subCategoryName}</Item>}
           </ComboBox>
         )}
+        {!!topics?.length && (
+          <ComboBox
+            items={topics}
+            placeholder="Select Topic(s)"
+            onSelectionChange={handleTopicChange}
+            >
+            {(item) => <Item key={item.topicId}>{item.topicName}</Item>}
+          </ComboBox>
+        )}
+        {!!selectedTopics && !!selectedTopics.length && (
+          <TopicsWrapper>
+            {selectedTopics.map((item, idx) => (
+              <div key={idx}>
+                <Button onPress={() => handleDeleteTopic(item.topicId)}>
+                  <span>{item.topicName}</span>
+                  {` `}
+                  <FcFullTrash />
+                </Button>
+              </div>
+            ))}
+          </TopicsWrapper>
+        )}
         {state.selected && (
           <>
-            <TextField width="auto" label="Description" />
-            <TextArea width="auto" label="Requirements" />
-            <Button variant="cta">Create</Button>
+            <TextField onChange={handleDescChange} width="auto" label="Description" />
+            <TextArea onChange={handleReqChange} width="auto" label="Requirements" />
+            <Button onPress={handleSubmitNewBounty} variant="cta">Create</Button>
           </>
         )}
     </BountyFormWrapper>
